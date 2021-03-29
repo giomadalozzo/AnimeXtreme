2bd72ac83800c920d40b9f995f62dba9d7a5129a
Giovanni <giomadalozzo@gmail.com>
2021-03-29 15:40:28 -0300

    First commit

---
+++ AnimExtreme/AnimExtreme/AnimExtremeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimExtremeApp.swift
+//  AnimExtreme
+//
+//  Created by Giovanni Madalozzo on 29/03/21.
+//
+
+import SwiftUI
+
+@main
+struct AnimExtremeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
