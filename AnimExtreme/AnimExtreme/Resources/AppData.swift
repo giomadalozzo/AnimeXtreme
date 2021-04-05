6770deb212c5dd60aff211d149a8e80f52c74268
Caroline Varela Ribeiro <cvr903@gmail.com>
2021-04-05 12:33:29 -0300

    Troca de imagens da lista de lojas

--- AnimExtreme/AnimExtreme/Resources/AppData.swift
+++ AnimExtreme/AnimExtreme/Resources/AppData.swift
@@ -37,14 +37,14 @@ var events: [Event] = [(Event(id: 001, name: "Desfile Cosplay", time: "10h", img
 var guests: [Guest] = [(Guest(id: 001, name: "Mandy Candy", info: "Info about guest", imgName: "mandyCandy")), (Guest(id: 002, name: "Tio Orochi", info: "Info about guest", imgName: "tioOrochi")), (Guest(id: 003, name: "Julio Cocielo", info: "Info about guest", imgName: "julioCocielo")), (Guest(id: 004, name: "Kim Rosacuca", info: "Info about guest", imgName: "kimRosacuca")), (Guest(id: 005, name: "Morimura", info: "Info about guest", imgName: "morimura")), (Guest(id: 006, name: "Luba Tv", info: "Info about guest", imgName: "lubaTV"))]
 var scheduleDays: [ScheduleInfo] = [(ScheduleInfo(id: 001, date: "(16/10)", weekDate: "Sábado")), (ScheduleInfo(id: 002, date: "(17/10)", weekDate: "Domingo"))]
 
-var shops: [Shop] = [Shop(id: 0, name: "Kitten", imgName: "shopKitten"),
-                     Shop(id: 1, name: "Anime Play", imgName: "shopKitten"),
-                     Shop(id: 2, name: "Minha Marcka", imgName: "shopKitten"),
-                     Shop(id: 3, name: "Crazy Animes", imgName: "shopKitten"),
-                     Shop(id: 4, name: "Fenix Rock Club", imgName: "shopKitten"),
-                     Shop(id: 5, name: "Leco's Shouten", imgName: "shopKitten"),
-                     Shop(id: 6, name: "Loja do Heroi", imgName: "shopKitten"),
-                     Shop(id: 7, name: "FGTec", imgName: "shopKitten"),
-                     Shop(id: 8, name: "Loja Anime Xtreme", imgName: "shopKitten"),
-                     Shop(id: 9, name: "Pharaoh's Shop", imgName: "shopKitten")
+var shops: [Shop] = [Shop(id: 01, name: "Kitten", imgName: "shopKitten"),
+                     Shop(id: 02, name: "Anime Play", imgName: "shopAnimePlay"),
+                     Shop(id: 03, name: "Minha Marcka", imgName: "shopMinhaMarcka"),
+                     Shop(id: 04, name: "Crazy Animes", imgName: "shopCrazyAnimes"),
+                     Shop(id: 05, name: "Fenix Rock Club", imgName: "shopFenixRockClub"),
+                     Shop(id: 06, name: "Leco's Shouten", imgName: "shopLecosShouten"),
+                     Shop(id: 07, name: "Loja do Heroi", imgName: "shopLojadoHeroi"),
+                     Shop(id: 08, name: "FGTec", imgName: "shopFGTec"),
+                     Shop(id: 09, name: "Loja Anime Xtreme", imgName: "shopAnimeXtreme"),
+                     Shop(id: 10, name: "Pharaoh's Shop", imgName: "shopPharaosShop")
 ]
